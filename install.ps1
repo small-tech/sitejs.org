a6e61245b1719cab3d7952c98f7b2d77e31c3751
Aral Balkan <mail@ar.al>
2020-06-10 10:15:44 +0100

    Clean up installation and package files

--- install.ps1
+++ install.ps1
@@ -1,13 +1,7 @@
-Write-Output "Installing site.js site for development…"
+Write-Output "Installing Site.js web site for development…"
 
 # Install build dependencies.
 Write-Output " > Build dependencies"
 npm install --global minify
-npm install --global imagemin-cli
-npm install --global svg-sprite
-
-# Install Node dependencies.
-Write-Output " > Node dependencies."
-npm install
 
 Write-Output " Done."
